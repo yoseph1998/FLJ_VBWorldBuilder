38e127d404afa4da0f4d53d7d94811fc9f4ef67d
Yoseph Alabdulwahab <yosephsa0@gmail.com>
2017-07-03 12:16:07 -0500

    Added Boilerplate program

---
+++ WorldBuilder/Form1.Designer.vb
@@ -0,0 +1,415 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TxtTextView = New System.Windows.Forms.TextBox()
+        Me.BtnGenerate = New System.Windows.Forms.Button()
+        Me.NumTilesX = New System.Windows.Forms.NumericUpDown()
+        Me.NumTilesY = New System.Windows.Forms.NumericUpDown()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.NumForest = New System.Windows.Forms.NumericUpDown()
+        Me.NumMountain = New System.Windows.Forms.NumericUpDown()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.NumDesert = New System.Windows.Forms.NumericUpDown()
+        Me.NumGrassland = New System.Windows.Forms.NumericUpDown()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.NumRivers = New System.Windows.Forms.NumericUpDown()
+        Me.NumSwamp = New System.Windows.Forms.NumericUpDown()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.NumLakes = New System.Windows.Forms.NumericUpDown()
+        Me.BtnRandomize = New System.Windows.Forms.Button()
+        Me.PicReference = New System.Windows.Forms.PictureBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown()
+        Me.ChkShowText = New System.Windows.Forms.CheckBox()
+        Me.PgsText = New System.Windows.Forms.ProgressBar()
+        CType(Me.NumTilesX, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumTilesY, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumForest, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumMountain, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumDesert, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumGrassland, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumRivers, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumSwamp, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumLakes, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PicReference, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TxtTextView
+        '
+        Me.TxtTextView.Location = New System.Drawing.Point(32, 22)
+        Me.TxtTextView.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.TxtTextView.MaxLength = 2147481599
+        Me.TxtTextView.Multiline = True
+        Me.TxtTextView.Name = "TxtTextView"
+        Me.TxtTextView.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TxtTextView.Size = New System.Drawing.Size(814, 820)
+        Me.TxtTextView.TabIndex = 0
+        Me.TxtTextView.WordWrap = False
+        '
+        'BtnGenerate
+        '
+        Me.BtnGenerate.Location = New System.Drawing.Point(1039, 964)
+        Me.BtnGenerate.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.BtnGenerate.Name = "BtnGenerate"
+        Me.BtnGenerate.Size = New System.Drawing.Size(182, 70)
+        Me.BtnGenerate.TabIndex = 1
+        Me.BtnGenerate.Text = "Generate"
+        Me.BtnGenerate.UseVisualStyleBackColor = True
+        '
+        'NumTilesX
+        '
+        Me.NumTilesX.Location = New System.Drawing.Point(167, 864)
+        Me.NumTilesX.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.NumTilesX.Maximum = New Decimal(New Integer() {4096, 0, 0, 0})
+        Me.NumTilesX.Name = "NumTilesX"
+        Me.NumTilesX.Size = New System.Drawing.Size(147, 31)
+        Me.NumTilesX.TabIndex = 2
+        Me.NumTilesX.Value = New Decimal(New Integer() {32, 0, 0, 0})
+        '
+        'NumTilesY
+        '
+        Me.NumTilesY.Location = New System.Drawing.Point(167, 901)
+        Me.NumTilesY.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.NumTilesY.Maximum = New Decimal(New Integer() {4096, 0, 0, 0})
+        Me.NumTilesY.Name = "NumTilesY"
+        Me.NumTilesY.Size = New System.Drawing.Size(147, 31)
+        Me.NumTilesY.TabIndex = 3
+        Me.NumTilesY.Value = New Decimal(New Integer() {32, 0, 0, 0})
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(28, 867)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(76, 23)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "TilesX"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(28, 904)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(76, 23)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "TilesY"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(337, 899)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(98, 23)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Forest %"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(337, 862)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(120, 23)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Mountain %"
+        '
+        'NumForest
+        '
+        Me.NumForest.Location = New System.Drawing.Point(477, 896)
+        Me.NumForest.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.NumForest.Name = "NumForest"
+        Me.NumForest.Size = New System.Drawing.Size(147, 31)
+        Me.NumForest.TabIndex = 7
+        '
+        'NumMountain
+        '
+        Me.NumMountain.Location = New System.Drawing.Point(477, 859)
+        Me.NumMountain.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.NumMountain.Name = "NumMountain"
+        Me.NumMountain.Size = New System.Drawing.Size(147, 31)
+        Me.NumMountain.TabIndex = 6
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(337, 973)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(98, 23)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "Desert %"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(337, 937)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(131, 23)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Grassland %"
+        '
+        'NumDesert
+        '
+        Me.NumDesert.Location = New System.Drawing.Point(477, 971)
+        Me.NumDesert.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.NumDesert.Name = "NumDesert"
+        Me.NumDesert.Size = New System.Drawing.Size(147, 31)
+        Me.NumDesert.TabIndex = 11
+        '
+        'NumGrassland
+        '
+        Me.NumGrassland.Location = New System.Drawing.Point(477, 934)
+        Me.NumGrassland.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.NumGrassland.Name = "NumGrassland"
+        Me.NumGrassland.Size = New System.Drawing.Size(147, 31)
+        Me.NumGrassland.TabIndex = 10
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(664, 861)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(76, 23)
+        Me.Label7.TabIndex = 17
+        Me.Label7.Text = "Rivers"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(337, 1011)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(87, 23)
+        Me.Label8.TabIndex = 16
+        Me.Label8.Text = "Swamp %"
+        '
+        'NumRivers
+        '
+        Me.NumRivers.Location = New System.Drawing.Point(867, 856)
+        Me.NumRivers.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.NumRivers.Name = "NumRivers"
+        Me.NumRivers.Size = New System.Drawing.Size(147, 31)
+        Me.NumRivers.TabIndex = 15
+        '
+        'NumSwamp
+        '
+        Me.NumSwamp.Location = New System.Drawing.Point(477, 1009)
+        Me.NumSwamp.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.NumSwamp.Name = "NumSwamp"
+        Me.NumSwamp.Size = New System.Drawing.Size(147, 31)
+        Me.NumSwamp.TabIndex = 14
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(664, 898)
+        Me.Label9.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(65, 23)
+        Me.Label9.TabIndex = 19
+        Me.Label9.Text = "Lakes"
+        '
+        'NumLakes
+        '
+        Me.NumLakes.Location = New System.Drawing.Point(867, 893)
+        Me.NumLakes.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.NumLakes.Name = "NumLakes"
+        Me.NumLakes.Size = New System.Drawing.Size(147, 31)
+        Me.NumLakes.TabIndex = 18
+        '
+        'BtnRandomize
+        '
+        Me.BtnRandomize.Location = New System.Drawing.Point(167, 964)
+        Me.BtnRandomize.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.BtnRandomize.Name = "BtnRandomize"
+        Me.BtnRandomize.Size = New System.Drawing.Size(147, 70)
+        Me.BtnRandomize.TabIndex = 22
+        Me.BtnRandomize.Text = "Randomize"
+        Me.BtnRandomize.UseVisualStyleBackColor = True
+        '
+        'PicReference
+        '
+        Me.PicReference.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PicReference.Location = New System.Drawing.Point(896, 22)
+        Me.PicReference.Name = "PicReference"
+        Me.PicReference.Size = New System.Drawing.Size(384, 384)
+        Me.PicReference.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PicReference.TabIndex = 23
+        Me.PicReference.TabStop = False
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(664, 992)
+        Me.Label10.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(153, 23)
+        Me.Label10.TabIndex = 27
+        Me.Label10.Text = "Tropic Zone %"
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(867, 987)
+        Me.NumericUpDown1.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(147, 31)
+        Me.NumericUpDown1.TabIndex = 26
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(664, 955)
+        Me.Label11.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(175, 23)
+        Me.Label11.TabIndex = 25
+        Me.Label11.Text = "Arctid Radius %"
+        '
+        'NumericUpDown2
+        '
+        Me.NumericUpDown2.Location = New System.Drawing.Point(867, 950)
+        Me.NumericUpDown2.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.NumericUpDown2.Name = "NumericUpDown2"
+        Me.NumericUpDown2.Size = New System.Drawing.Size(147, 31)
+        Me.NumericUpDown2.TabIndex = 24
+        '
+        'ChkShowText
+        '
+        Me.ChkShowText.AutoSize = True
+        Me.ChkShowText.Checked = True
+        Me.ChkShowText.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.ChkShowText.Location = New System.Drawing.Point(32, 971)
+        Me.ChkShowText.Name = "ChkShowText"
+        Me.ChkShowText.Size = New System.Drawing.Size(124, 27)
+        Me.ChkShowText.TabIndex = 28
+        Me.ChkShowText.Text = "ShowText"
+        Me.ChkShowText.UseVisualStyleBackColor = True
+        '
+        'PgsText
+        '
+        Me.PgsText.BackColor = System.Drawing.Color.Red
+        Me.PgsText.ForeColor = System.Drawing.Color.Gold
+        Me.PgsText.Location = New System.Drawing.Point(59, 405)
+        Me.PgsText.Name = "PgsText"
+        Me.PgsText.Size = New System.Drawing.Size(732, 68)
+        Me.PgsText.TabIndex = 29
+        Me.PgsText.Visible = False
+        '
+        'FrmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 23.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1705, 1060)
+        Me.Controls.Add(Me.PgsText)
+        Me.Controls.Add(Me.ChkShowText)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.NumericUpDown2)
+        Me.Controls.Add(Me.PicReference)
+        Me.Controls.Add(Me.BtnRandomize)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.NumLakes)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.NumRivers)
+        Me.Controls.Add(Me.NumSwamp)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.NumDesert)
+        Me.Controls.Add(Me.NumGrassland)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.NumForest)
+        Me.Controls.Add(Me.NumMountain)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.NumTilesY)
+        Me.Controls.Add(Me.NumTilesX)
+        Me.Controls.Add(Me.BtnGenerate)
+        Me.Controls.Add(Me.TxtTextView)
+        Me.Font = New System.Drawing.Font("Consolas", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Name = "FrmMain"
+        Me.Text = "World Builder"
+        CType(Me.NumTilesX, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumTilesY, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumForest, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumMountain, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumDesert, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumGrassland, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumRivers, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumSwamp, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumLakes, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PicReference, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TxtTextView As TextBox
+    Friend WithEvents BtnGenerate As Button
+    Friend WithEvents NumTilesX As NumericUpDown
+    Friend WithEvents NumTilesY As NumericUpDown
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents NumForest As NumericUpDown
+    Friend WithEvents NumMountain As NumericUpDown
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents NumDesert As NumericUpDown
+    Friend WithEvents NumGrassland As NumericUpDown
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents NumRivers As NumericUpDown
+    Friend WithEvents NumSwamp As NumericUpDown
+    Friend WithEvents Label9 As Label
+    Friend WithEvents NumLakes As NumericUpDown
+    Friend WithEvents BtnRandomize As Button
+    Friend WithEvents PicReference As PictureBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents Label11 As Label
+    Friend WithEvents NumericUpDown2 As NumericUpDown
+    Friend WithEvents ChkShowText As CheckBox
+    Friend WithEvents PgsText As ProgressBar
+End Class
---
+++ WorldBuilder/Form1.vb
@@ -0,0 +1,151 @@
+﻿Imports System.Drawing.Drawing2D
+Imports System.Text
+
+Public Class FrmMain
+
+    Dim GblAryIntMap(0, 0) As TileType
+    Dim GblIntSizeX As Integer = 0
+    Dim GblIntSizeY As Integer = 0
+    Dim GblColTiles(12) As Color
+    Dim GblIntZoom As Integer = 12
+
+    Enum TileType As Integer
+        Null = 0
+        Water = 1
+        Ice = 2
+        Grass = 3
+        Snow = 4
+        Desert = 5
+        Forest = 6
+        Jungle = 7
+        SnowForest = 8
+        Mountain = 9
+        SnowMountain = 10
+        Swamp = 11
+    End Enum
+    Private Sub FrmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        GblColTiles(0) = Color.Black
+        GblColTiles(1) = Color.Blue
+        GblColTiles(2) = Color.LightCyan
+        GblColTiles(3) = Color.LawnGreen
+        GblColTiles(4) = Color.White
+        GblColTiles(5) = Color.SandyBrown
+        GblColTiles(6) = Color.Green
+        GblColTiles(7) = Color.Olive
+        GblColTiles(8) = Color.PaleGreen
+        GblColTiles(9) = Color.Brown
+        GblColTiles(10) = Color.Tan
+        GblColTiles(11) = Color.Fuchsia
+
+    End Sub
+
+    Private Sub BtnGenerate_Click(sender As Object, e As EventArgs) Handles BtnGenerate.Click
+        'UPDATE VARIABLES
+        GblIntSizeX = NumTilesX.Value
+        GblIntSizeY = NumTilesY.Value
+        ReDim GblAryIntMap(GblIntSizeX, GblIntSizeY)
+
+        'CREATE TILEMAP
+        CreateTileMap() '<--- Do your work here
+
+        'THIS SHOWS THE RESILTS 
+        TxtTextView.Text = GenerateTextMap()
+        PicReference.Image = GeneratePixelMap()
+    End Sub
+
+    Private Function GeneratePixelMap() As Bitmap
+        Dim BmpReturn As New Bitmap(GblIntSizeX, GblIntSizeY)
+        Dim IntColorIndex As Integer
+
+        For IntTileY = 0 To GblIntSizeY - 1 Step 1
+            For IntTileX = 0 To GblIntSizeX - 1 Step 1
+                IntColorIndex = GblAryIntMap(IntTileX, IntTileY)
+                BmpReturn.SetPixel(IntTileX, IntTileY, GblColTiles(IntColorIndex))
+            Next IntTileX
+        Next IntTileY
+
+        Return BmpReturn
+    End Function
+
+    Private Sub CreateTileMap()
+        'DECLARE VARIABLES
+        Dim IntTileX As Integer
+        Dim IntTileY As Integer
+
+        'I CREATED RANDOM TILES
+        'YOUR CODE SHOLD CREATE STRUCTURES THAT CONTINENTS, OCEANS, ISLANDS, ETC
+
+        For IntTileY = 0 To GblIntSizeY - 1 Step 1
+            For IntTileX = 0 To GblIntSizeX - 1 Step 1
+                GblAryIntMap(IntTileX, IntTileY) = Choose({1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11})
+            Next IntTileX
+        Next IntTileY
+    End Sub
+
+    Function GenerateTextMap() As String
+        'DECLARE VARIABLES
+        Dim IntTileX As Integer
+        Dim IntTileY As Integer
+        Dim StrReturn As New StringBuilder
+        Dim IntTileType As Integer
+        Dim StrTileType As String
+
+
+
+        PgsText.Value = 0
+        PgsText.Visible = True
+        PgsText.Maximum = (GblIntSizeX) * (GblIntSizeY)
+
+
+        'LOOP THROUGH ARRAY AND WRITE EACH INDEX TO STRING
+        For IntTileY = 0 To GblIntSizeY - 1 Step 1
+            For IntTileX = 0 To GblIntSizeX - 1 Step 1
+                IntTileType = GblAryIntMap(IntTileX, IntTileY)
+                StrTileType = IntTileType.ToString.PadLeft(2, "0") + " "
+                StrReturn.Append(StrTileType)
+                PgsText.Value += 1
+            Next IntTileX
+            StrReturn.Append(vbNewLine)
+            Me.Invalidate()
+        Next IntTileY
+
+        PgsText.Visible = False
+        Return StrReturn.ToString
+    End Function
+
+    Function Choose(ObjAryChoices() As Object) As Object
+        Dim IntMaxIndex As Integer
+        Dim IntSelectedIndex = 0
+
+        'CHOOSE A VALUE FROM AN ARRAY AT RANDOM
+        IntMaxIndex = ObjAryChoices.Length - 1
+        IntSelectedIndex = CInt(Rnd() * IntMaxIndex)
+
+        Return ObjAryChoices(IntSelectedIndex)
+    End Function
+
+
+
+
+    Private Sub PicReference_Click(sender As Object, e As MouseEventArgs) Handles PicReference.Click
+        If e.Button = MouseButtons.Left Then GblIntZoom += 1
+        If e.Button = MouseButtons.Right Then GblIntZoom -= 1
+        If e.Button = MouseButtons.Middle Then GblIntZoom = 1
+
+        PicReference.Width = GblIntSizeX * GblIntZoom
+        PicReference.Height = GblIntSizeY * GblIntZoom
+        PicReference.Invalidate()
+
+    End Sub
+
+    Private Sub PicReference_Paint(sender As Object, e As PaintEventArgs) Handles PicReference.Paint
+        Dim GrpTemp As Graphics
+        GrpTemp = e.Graphics
+        GrpTemp.InterpolationMode = InterpolationMode.NearestNeighbor
+        GrpTemp.PixelOffsetMode = PixelOffsetMode.Half
+
+        If PicReference.Image Is Nothing = False Then
+            GrpTemp.DrawImage(PicReference.Image, 0, 0, PicReference.Width, PicReference.Height)
+        End If
+    End Sub
+End Class
